170845ff1d71381edcaa309c328c00f6c13ad5fb
Alex da Franca <alex.dafranca@dpdhl.com>
2021-06-11 23:25:05 +0200

    removed wrong comment

--- xcresultparser/CoverageConverter.swift
+++ xcresultparser/CoverageConverter.swift
@@ -8,26 +8,6 @@
 import Foundation
 import XCResultKit
 
-/// we need to convert the format which xccov retrieves from xcresult to the
-/// format required for sonarqube in xml format
-/// Both formats are pretty different. Only an approximation can be made
-/// as the xcresult format doesn't break it down to single lines
-/// instead it lists functions with only their number of executable lines
-/// and the number of covered lines, so that it is not clear, which line exactly
-/// is covered. We know the linenumber of the function start and can "count" from there onwards
-/// Additionally the listing of functions is linear, while in fact its format is nested:
-/// Closures in a function have their startin linenumber within the range of the
-/// enclosing function.
-///
-/// The sonarqube format however just wants a list of line numbers and a boolean value
-/// whether the line was covered once or not.
-/// (while xcresult carries info about how often the function was covered)
-///
-/// So this is just a naive approach of listing line numbers with the
-/// right proportion of covered and uncovered lines.
-/// While it looks like the format into which we convert knows, which line exactly
-/// is covered and which not, in reality it is an assumption based on the informatioin we have in "runs" of covered/uncovered lines.
-
 struct CoverageConverter {
     private let resultFile: XCResultFile
     private let projectRoot: String
