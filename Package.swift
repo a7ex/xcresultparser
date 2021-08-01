908c13677e1280dbba7c6314e43aa5369119863a
Dariusz Rybicki <darrarski@gmail.com>
2021-08-01 19:39:46 +0200

    Migrate Xcode project to Swift Package

---
+++ Package.swift
@@ -0,0 +1,43 @@
+// swift-tools-version:5.4
+import PackageDescription
+
+let package = Package(
+    name: "xcresultparser",
+    platforms: [
+        .macOS(.v11),
+    ],
+    products: [
+        .executable(
+            name: "xcresultparser",
+            targets: ["xcresultparser"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            name: "swift-argument-parser",
+            url: "https://github.com/apple/swift-argument-parser.git",
+            .exact("0.4.3")
+        ),
+        .package(
+            name: "XCResultKit",
+            url: "https://github.com/davidahouse/XCResultKit.git",
+            .exact("0.9.1")
+        ),
+    ],
+    targets: [
+        .executableTarget(
+            name: "xcresultparser",
+            dependencies: [
+                .product(
+                    name: "ArgumentParser",
+                    package: "swift-argument-parser"
+                ),
+                .product(
+                    name: "XCResultKit",
+                    package: "XCResultKit"
+                ),
+            ],
+            path: "xcresultparser"
+        ),
+    ]
+)
